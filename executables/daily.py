2a69f02acabbd385558a72730599d3ac74c1e9e5
lasse <lasse-schloer@servermx.de>
2021-08-25 15:08:49 +0200

    finally got daily.py right

--- executables/daily.py
+++ executables/daily.py
@@ -6,17 +6,22 @@ import datetime
 import time
 import re
 import os
-import shutil
 import sys
 import tarfile
 import lzma
 
 # Parameters
-inpath = os.path.join("/", "home", "pi", "sensor-logging", "data",
+inpath = os.path.join("/", "home", "schloer", "sensor-logging", "data",
         "minly-copy-delme")
-outpath = os.path.join("/", "home", "pi", "sensor-logging", "data", "daily")
+outpath = os.path.join("/", "home", "schloer", "sensor-logging", "data", "daily")
 devices = ["dht22", "sensorhub"]
 min_age_days = 2
+tar_format = tarfile.GNU_FORMAT
+lzma_format = lzma.FORMAT_XZ
+lzma_check = lzma.CHECK_CRC64
+lzma_filters = [{"id": lzma.FILTER_LZMA2, "dict_size": 16777216,
+                    "lc": 4, "lp": 0, "pb": 0, "mf": lzma.MF_HC4, "mode":
+                    lzma.MODE_NORMAL, "nice_len": 100, "depth": 10}]
 
 
 # Calculate threshold date and gather candidate files
@@ -34,10 +39,14 @@ print(f"\"{sys.argv[0]}\" preparing at GMT " +
         time.strftime("%Y-%m-%d-%H-%M-%S.", time.gmtime()))
 print(sep)
 print("Parameters:")
-print(f"`inpath`: '{inpath}'")
-print(f"`outpath`: '{outpath}'")
-print(f"`devices`: {devices}")
-print(f"`min_age_days`: {min_age_days}")
+print(f"  `inpath`: '{inpath}'")
+print(f"  `outpath`: '{outpath}'")
+print(f"  `devices`: {devices}")
+print(f"  `min_age_days`: {min_age_days}")
+print(f"  `tar_format`: {tar_format}")
+print(f"  `lzma_format`: {lzma_format}")
+print(f"  `lzma_check`: {lzma_check}")
+print(f"  `lzma_filters`: {lzma_filters}")
 print(sep)
 print(f"`min_age_date`: {min_age_date.isoformat()}")
 print(f"Found {len(files)} candidate files in inpath to potentially archive.")
@@ -50,36 +59,37 @@ matches = [m for m in matches if not (m is None)]
 matches = [m for m in matches if to_date(m) <= min_age_date]
 
 # Partition into days and archive
-for (k, g) in itertools.groupby(sorted(matches, key=to_date), to_date):
+parts = itertools.groupby(sorted(matches, key=to_date), to_date)
+for (i, (k, g)) in enumerate(parts):
     infiles = sorted([m.group() for m in g])
-    outfile = f"{k.isoformat()}"
-    print(f"Processing {len(infiles)} files for date {k.isoformat()}…")
+    outfile = f"{k.isoformat()}.tar.xz"
+    print(f"Part {i}:")
+    print("  Processing {len(infiles)} files for date {k.isoformat()}…")
 
-    # Make a temporary directory and copy relevant files there
-    # The reason for this is that tar behaves favorably when archiving a whole
-    # directory as compared to a list of files
+    try:
+        tic = time.time()
+        with lzma.open(f"{os.path.join(outpath, outfile)}", "wb",
+                format=lzma_format, check=lzma_check,
+                filters=lzma_filters) as xz:
+            with tarfile.open(None, "w", xz, format=tar_format) as tar:
+                for infile in infiles:
+                    tar.add(os.path.join(inpath, infile), arcname=infile)
+        toc = time.time()
+    except Exception as e:
+        print(f"  Creating archive failed: {e}")
+    else:
+        print("  Creating archive took {:.2f} seconds.".format(toc - tic))
+        print("  Deleting uncompressed files…")
+        try:
+            tic = time.time()
+            for infile in infiles:
+                os.remove(os.path.join(inpath, infile))
+            toc = time.time()
+        except Exception as e:
+            print(f"  Deleting uncompressed files failed: {e}")
+        else:
+            print("  Deleting took {:.2f} seconds.".format(toc - tic))
 
-    # Note: Oh boy… It seems that a whole lot of the trial and error stuff I did
-    # turned out as bullshit because my minly-copy-delme data was corrupt for
-    # whatever reason. So I guess the whole thing might be easier than I
-    # thought, and I may not even need to create an extra directory before
-    # archiving…
-
-    tic0 = time.time()
-    os.mkdir(os.path.join(outpath, outfile))
-    for infile in infiles:
-        shutil.copy(os.path.join(inpath, infile), os.path.join(outpath, outfile))
-    toc0 = time.time()
-    print("Copying took {:.2f} seconds.".format(toc0 - tic0))
-
-    tic1 = time.time()
-    with tarfile.open(f"{os.path.join(outpath, outfile)}.tar", "w",
-            format=tarfile.USTAR_FORMAT) as tar:
-        tar.add(os.path.join(outpath, outfile), arcname=outfile)
-    toc1 = time.time()
-    print("Creating tar archive took {:.2f} seconds.".format(toc1 - tic1))
-
-
-print(f"# Process \"{sys.argv[0]}\" completing at GMT " +
+print(f"\"{sys.argv[0]}\" completing at GMT " +
         time.strftime("%Y-%m-%d-%H-%M-%S.\n", time.gmtime()))
 
