8443e0a55bff2c29580b9238dbbf4ec5ccc84b47
Lasse Schlör <lasse-schloer@servermx.de>
2021-08-29 14:40:52 +0200

    made script ready for "production"

--- executables/daily.py
+++ executables/daily.py
@@ -11,17 +11,23 @@ import tarfile
 import lzma
 
 # Parameters
-inpath = os.path.join("/", "home", "schloer", "sensor-logging", "data",
-        "minly-copy-delme")
-outpath = os.path.join("/", "home", "schloer", "sensor-logging", "data", "daily")
+inpath = os.path.join("/", "home", "pi", "sensor-logging", "data", "minly")
+outpath = os.path.join("/", "home", "pi", "sensor-logging", "data", "daily")
 devices = ["dht22", "sensorhub"]
-min_age_days = 2
+min_age_days = 2 # 2 for security, should there ever be some dating issues
 tar_format = tarfile.GNU_FORMAT
 lzma_format = lzma.FORMAT_XZ
 lzma_check = lzma.CHECK_CRC64
+# Note: The maximum nice value of 273 seems to produce the best results.
+# Increasing depth increases the runtime approximately linearly, but squeezes a
+# few KiB more out of each archive. Since the Raspberry Pi hangs around with
+# idle CPUs anyway, I don't think it's a problem to run an expensive compression
+# once daily for a few minutes for this admittedly minor improvement in file
+# size. I only see very minor improvements when increasing the depth from 200 to
+# 300, so I guess I won't bother to go even higher.
 lzma_filters = [{"id": lzma.FILTER_LZMA2, "dict_size": 16777216,
                     "lc": 4, "lp": 0, "pb": 0, "mf": lzma.MF_HC4, "mode":
-                    lzma.MODE_NORMAL, "nice_len": 100, "depth": 10}]
+                    lzma.MODE_NORMAL, "nice_len": 273, "depth": 300}]
 
 
 # Calculate threshold date and gather candidate files
@@ -61,10 +67,23 @@ matches = [m for m in matches if to_date(m) <= min_age_date]
 # Partition into days and archive
 parts = itertools.groupby(sorted(matches, key=to_date), to_date)
 for (i, (k, g)) in enumerate(parts):
+    # Note: I could probably squeeze a lot higher compression ratios out of this
+    # data if I stored it in a custom binary format and then used some FLAC-like
+    # compression or at least a delta filter. Since this would take quite some
+    # coding effort and perhaps even create maintenance and compatibility
+    # issues, I won't go overboard with that and just use LZMA on the CSVs. I'll
+    # end up with some 300 MiB of compressed data per year, which I think is
+    # acceptable.
+    # Note: I tried using the xz delta filter, but it seems to produce worse
+    # results, even if I limit the input to one device. One issue is that the
+    # CSV entries don't always have the same length – maybe delta compression
+    # would be better if they did.
+    # Note: Sorting infiles improves compression. Sorting by timestamp first
+    # seems to produce favorable results as compared to sorting by device first.
     infiles = sorted([m.group() for m in g])
     outfile = f"{k.isoformat()}.tar.xz"
     print(f"Part {i}:")
-    print("  Processing {len(infiles)} files for date {k.isoformat()}…")
+    print(f"  Processing {len(infiles)} files for date {k.isoformat()}…")
 
     try:
         tic = time.time()
