65c62afdd6f893111a33ea4749e5fa146da1cdc6
Lasse Schlör <lasse-schloer@servermx.de>
2023-07-04 21:02:19 +0200

    got sensorhub reading and aggregation working

--- script/write-sensors-include.py
+++ script/write-sensors-include.py
@@ -5,45 +5,255 @@ import textwrap
 def indent(str, n = 1):
   return textwrap.indent(str, "  " * n)
 
+# def expand(sensors):
+#   "Bring JSON spec into a canonical form"
+#   for sensor_name, sensor_params in sensors.items():
+#     for field_name, field_params in sensor_params.items():
+#       if isinstance(field_params["aggregate"], str):
+#         field_params["aggregate"] = {
+#           "type": field_params["aggregate"], "unless": []}
+#   return sensors
+
+def snippet_bool_as_csv_string():
+  return textwrap.dedent('''\
+    std::string_view bool_as_csv_string(bool const x) {
+      return std::string_view{x ? "1" : "0"};
+    }
+    ''')
+
+# def snippet_optional_apply():
+#   return textwrap.dedent('''\
+#     // NOTE: `optional_apply` could perhaps be defined as a variadic template
+#     // and function, but let's keep it simple for now.
+#
+#     template<class F, class T>
+#     std::optional<T> optional_apply(F f, std::optional<T> const &x0) {
+#       if (x0.has_value()) return std::optional<T>{f(x0.value())};
+#       else return std::optional<T>{};
+#     }
+#
+#     template<class F, class T>
+#     std::optional<T> optional_apply(F f,
+#         std::optional<T> const &x0, std::optional<T> const &x1) {
+#       if (x0.has_value()) {
+#         if (x1.has_value()) return std::optional<T>{f(x0.value(), x1.value())};
+#         else return std::optional<T>{x0};
+#       } else {
+#         if (x1.has_value()) return std::optional<T>{x1};
+#         else return std::optional<T>{};
+#       }
+#     }
+#     ''')
+
+def snippet_aggregation_step_ops():
+  return textwrap.dedent('''\
+    auto constexpr aggregation_step_mean{
+      [](auto const &x0, auto const &x1){return x0 + x1;}};
+    auto constexpr aggregation_step_min{
+      [](auto const &x0, auto const &x1){return std::min(x0, x1);}};
+    auto constexpr aggregation_step_max{
+      [](auto const &x0, auto const &x1){return std::max(x0, x1);}};
+    ''')
+
+def snippet_write_field():
+  return textwrap.dedent('''\
+    template<class T>
+    std::ostream &write_field(std::ostream &out,
+        T const &field, std::optional<int> const decimals) {
+      if (decimals.has_value()) {
+        out << std::setprecision(decimals.value()) << std::fixed;
+      } else {
+        out << std::setprecision(cc::field_decimals_default)
+          << std::defaultfloat;
+      }
+      return out << field;
+    }
+
+    template<class T>
+    std::ostream &write_field(std::ostream &out,
+        std::optional<T> const &field, std::optional<int> const decimals) {
+      return field.has_value() ? write_field(out, field.value(), decimals)
+                               : out;
+    }
+
+    std::ostream &write_field(std::ostream &out,
+        std::string_view const &field, std::optional<int> const) {
+      return out << field;
+    }
+
+    std::ostream &write_field(std::ostream &out,
+        bool const &field, std::optional<int> const) {
+      return out << (field ? "1" : "0");
+    }
+    ''')
+
 def snippet_struct(sensor_name, sensor_params):
-  str = f'struct {sensor_name} : sensor {{\n'
+  str = f'struct {sensor_name} : public sensor {{\n'
   for field_name, field_params in sensor_params.items():
-    str += indent(f'std::optional<{field_params["type"]}> {field_name};\n')
+    str += indent(f'std::optional<{field_params["type"]}> {field_name}{{}};\n')
   return str + f'}};\n'
 
+def snippet_aggregation_state(sensor_name, sensor_params):
+  str = f'struct {sensor_name}_state : public aggregation_state {{\n'
+  for field_name, field_params in sensor_params.items():
+    if field_params["aggregate"] in ["mean"]:
+      str += indent(f'unsigned {field_name}_count{{0u}};\n')
+  return str + f'}};\n'
+
+def snippet_aggregation_step(sensor_name, sensor_params):
+  str = f'auto aggregation_step(\n'
+  str += indent(f'{sensor_name} const aggregate,\n', 2)
+  str += indent(f'{sensor_name}_state const state,\n', 2)
+  str += indent(f'{sensor_name} const sample) {{\n', 2)
+  for field_name, field_params in sensor_params.items():
+    str += indent(f'auto const {field_name}{{optional_apply('
+      f'aggregation_step_{field_params["aggregate"]},\n')
+    str += indent(f'aggregate.{field_name}, sample.{field_name})}};\n', 2)
+    if field_params["aggregate"] in ["mean"]:
+      str += indent(
+        f'auto const {field_name}_count{{state.{field_name}_count + 1u}};\n')
+
+  str += indent(
+    f'\nreturn std::pair<{sensor_name}, {sensor_name}_state>{{{{\n')
+  str += indent(f'{{}},\n', 3)
+  for field_name, field_params in sensor_params.items():
+    str += indent(f'{field_name},\n', 3)
+  str += indent(f'}}, {{\n', 2)
+  str += indent(f'{{}},\n', 3)
+  for field_name, field_params in sensor_params.items():
+    if field_params["aggregate"] in ["mean"]:
+      str += indent(f'{field_name}_count,\n', 3)
+  str += indent(f'}}\n', 2)
+  str += indent(f'}};\n')
+  return str + f'}}\n'
+
+def snippet_aggregation_finish(sensor_name, sensor_params):
+  str = f'auto aggregation_finish(\n'
+  str += indent(f'{sensor_name} const aggregate,\n', 2)
+  str += indent(f'{sensor_name}_state const state) {{\n', 2)
+  for field_name, field_params in sensor_params.items():
+    if field_params["aggregate"] in ["mean"]:
+      str += indent(
+        f'auto const {field_name}{{optional_apply([=](auto const &x){{\n', 2)
+      str += indent(f'return x / static_cast<{field_params["type"]}>('
+        f'state.{field_name}_count); }},\n', 3)
+      str += indent(f'aggregate.{field_name})}};\n', 3)
+    else:
+      str += indent(f'auto const {field_name}{{aggregate.{field_name}}};\n', 2)
+
+  str += indent(
+    f'\nreturn {sensor_name}{{\n')
+  str += indent(f'{{}},\n', 2)
+  for field_name, field_params in sensor_params.items():
+    str += indent(f'{field_name},\n', 2)
+  str += indent(f'}};\n')
+  return str + f'}}\n'
+
 def snippet_name(sensor_name, sensor_params):
-  str = f'std::string name({sensor_name} const) {{\n'
-  str += indent(f'return std::string{{"{sensor_name}"}};\n')
+  str = f'std::string_view name({sensor_name} const) {{\n'
+  str += indent(f'return std::string_view{{"{sensor_name}"}};\n')
   return str + f'}}\n'
 
 def snippet_field_names(sensor_name, sensor_params):
-  str = f'std::string field_names({sensor_name} const x) {{\n'
+  str = f'std::vector<std::string_view> field_names({sensor_name} const) {{\n'
   str += indent(f'return std::vector{{')
   for field_name, field_params in sensor_params.items():
-    str += indent(f'\nstd::string{{"{field_name}"}},', 2)
+    str += indent(f'\nstd::string_view{{"{field_name}"}},', 2)
   str += indent(f'\n}};\n')
   return str + f'}}\n'
+
+# def snippet_as_tuple(sensor_name, sensor_params):
+#   str = f'auto as_tuple({sensor_name} const data) {{\n'
+#   str += indent(f'return std::tuple{{\n')
+#   for field_name, field_params in sensor_params.items():
+#     str += indent(f'data.{field_name},\n', 2)
+#   str += indent(f'}};\n')
+#   return str + f'}}\n'
+
+# def snippet_field_decimals(sensor_name, sensor_params):
+#   str = f'auto field_decimals({sensor_name} const) {{\n'
+#   str += indent(f'return std::tuple{{\n')
+#   for field_name, field_params in sensor_params.items():
+#     if "decimals" in field_params:
+#       str += indent(
+#         f'std::optional<int>{{{field_params["decimals"]}}},\n', 2)
+#     else:
+#       str += indent(f'std::optional<int>{{}},\n', 2)
+#   str += indent(f'}};\n')
+#   return str + f'}}\n'
+
+def snippet_write_fields(sensor_name, sensor_params):
+  str = f'std::ostream &write_fields(\n'
+  str += indent(f'std::ostream &out, {sensor_name} const data) {{\n', 2)
+  str += indent(f'auto const original_precision{{out.precision()}};')
+  is_first_entry = True
+  for field_name, field_params in sensor_params.items():
+    if is_first_entry:
+      is_first_entry = False
+    else:
+      str += indent(f'out << cc::csv_delimiter_default;\n')
+    str += indent(f'write_field(out, data.{field_name}, std::optional<int>{{')
+    if "decimals" in field_params:
+      str += f'{field_params["decimals"]}'
+    str += f'}});\n'
+  str += indent(f'return out << std::setprecision(original_precision)\n')
+  str += indent(f'<< std::defaultfloat << std::endl;\n', 2)
+  return str + f'}}\n'
+
+def snippet_write_field_names():
+  return textwrap.dedent('''\
+    template<class T>
+    std::ostream &write_field_names(std::ostream &out, T const &data) {
+      auto const &sensor_name{name(data)};
+      bool is_first_entry = true;
+      for (auto const &field_name : field_names(data)) {
+        if (is_first_entry) is_first_entry = false;
+        else out << cc::csv_delimiter_default;
+        out << "\\"" << sensor_name << "_" << field_name << "\\"";
+      }
+      return out << std::endl;
+    }
+    ''')
 
 def sensors_include(sensors):
   str = f''
   sep = f'\n'
 
-  includes = ["vector", "string", "optional"]
+  rel_file_path = os.path.relpath(__file__,
+    os.path.join(os.path.dirname(__file__), "..", "src"))
+  str += f'// File generated by `{rel_file_path}`\n' + sep
+
+  includes = ["vector", "string_view", "utility", "optional", "tuple",
+    "ostream", "iomanip", "ios", "algorithm"]
 
   for include in includes:
     str += f'#include <{include}>\n'
     pass
+  str += sep
 
-  str += sep + f'namespace sensors {{\n' + sep
-  str += indent(f'struct sensor {{}};\n') + sep
+  str += f'namespace cc {{\n' + sep
+  str += indent(f'int constexpr field_decimals_default{{6u}};\n' + sep)
+  str += indent(
+    f'std::string_view constexpr csv_delimiter_default{{", "}};\n' + sep)
+  str += f'}} // namespace cc\n' + sep
 
-  for sensor_name, sensor_params in sensors.items():
-    str += indent(snippet_struct(       sensor_name, sensor_params) + sep)
-    str += indent(snippet_name(         sensor_name, sensor_params) + sep)
-    str += indent(snippet_field_names(  sensor_name, sensor_params) + sep)
-    pass
+  str += f'namespace sensors {{\n' + sep
+  str += indent(f'struct sensor {{}};\n' + sep)
+  str += indent(f'struct aggregation_state {{}};\n' + sep)
+  str += indent(snippet_bool_as_csv_string() + sep)
+  # str += indent(snippet_optional_apply() + sep)
+  str += indent(snippet_aggregation_step_ops() + sep)
+  str += indent(snippet_write_field() + sep)
 
-  return str + f'}} // namespace sensors'
+  for snippet in [snippet_struct, snippet_aggregation_state,
+      snippet_aggregation_step, snippet_aggregation_finish, snippet_name,
+      snippet_field_names, snippet_write_fields]:
+    for sensor_name, sensor_params in sensors.items():
+      str += indent(snippet(sensor_name, sensor_params) + sep)
+
+  str += indent(snippet_write_field_names() + sep)
+
+  return str + f'}} // namespace sensors\n'
 
 sensors_json_filename = os.path.join(os.path.dirname(__file__), "sensors.json")
 sensors_cpp_filename = os.path.join(os.path.dirname(__file__), "..", "src",
@@ -51,6 +261,8 @@ sensors_cpp_filename = os.path.join(os.path.dirname(__file__), "..", "src",
 
 with open(sensors_json_filename, "r") as f:
   sensors = json.load(f)
+
+# sensors = expand(sensors)
 
 sensors_include_str = sensors_include(sensors)
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,244 @@
 #include <iostream>
+#include <ios>
 #include <chrono>
 #include <thread>
 #include <optional>
+#include <tuple>
 #include <string_view>
+#include <cstdint>
+#include <cstdlib>
 
 extern "C" {
   #include <pigpiod_if2.h>
 }
 
-#include "sensors.cpp"
+#include "machine.cpp"
 
 namespace cc { // compile-time constants
-  #include "machine.cpp"
 
-  bool constexpr use_gpio{true};
-  bool constexpr use_i2c{hostname == std::string_view{"lasse-raspberrypi-0"}};
+  bool constexpr log_errors{true};
 
-  std::chrono::milliseconds constexpr sampling_interval{250};
+  std::chrono::milliseconds constexpr sampling_interval{300};
   std::chrono::milliseconds constexpr sleeping_{10};
-  unsigned constexpr samples_per_aggregate{6u};
-  unsigned constexpr aggregates_per_run{2u};
+  unsigned constexpr samples_per_aggregate{5u};
+  unsigned constexpr aggregates_per_run{4u};
+}
+
+// NOTE: `optional_apply` could perhaps be defined as a variadic template
+// and function, but let's keep it simple for now. C++23 also has something like
+// this, but I'm on C++20 for now.
+template<class F, class T>
+std::optional<T> optional_apply(F f, std::optional<T> const &x0) {
+  if (x0.has_value()) return std::optional<T>{f(x0.value())};
+  else return std::optional<T>{};
+}
+template<class F, class T>
+std::optional<T> optional_apply(F f,
+    std::optional<T> const &x0, std::optional<T> const &x1) {
+  if (x0.has_value()) {
+    if (x1.has_value()) return std::optional<T>{f(x0.value(), x1.value())};
+    else return std::optional<T>{x0};
+  } else {
+    if (x1.has_value()) return std::optional<T>{x1};
+    else return std::optional<T>{};
+  }
+}
+
+struct Pi {
+  int const handle;
+  operator int() const { return this->handle; }
+
+  Pi(Pi const &) = delete;
+
+  Pi(char const *addr = nullptr, char const *port = nullptr) :
+      handle{pigpio_start(addr, port)} {
+    if (this->handle < 0) {
+      if constexpr (cc::log_errors) {
+        char const *port_env{std::getenv("PIGPIO_PORT")};
+        std::cerr << "Error connecting to pigpio daemon at "
+          << "address " << (addr ? addr : "localhost")
+          << ", port " << (port ? port : (port_env ? port_env : "8888"))
+          << ": " << pigpio_error(this->handle) << std::endl;
+      }
+    }
+  }
+
+  ~Pi() {
+    if (this->handle >= 0) pigpio_stop(this->handle);
+  }
+};
+
+struct I2C {
+  int const handle;
+  int const pi_handle;
+  operator int() const { return this->handle; }
+
+  I2C(I2C const &) = delete;
+
+  I2C(int const pi_handle, unsigned const bus,
+      unsigned const addr, unsigned const flags = 0u) : 
+      handle{i2c_open(pi_handle, bus, addr, flags)}, pi_handle{pi_handle} {
+    if (this->handle < 0) if constexpr (cc::log_errors) std::cerr
+      << "Error opening I2C device on "
+      << "Pi " << pi_handle
+      << ", bus 0x" << std::hex << bus << std::dec
+      << ", address 0x" << std::hex << addr << std::dec
+      << ", flags 0x" << std::hex << flags << std::dec
+      << ": " << pigpio_error(this->handle) << std::endl;
+  }
+
+  ~I2C() {
+    if (this->handle >= 0) {
+      int const response{i2c_close(this->pi_handle, this->handle)};
+      if (response < 0) if constexpr (cc::log_errors) std::cerr
+        << "Error closing I2C device on "
+        << "Pi " << pi_handle
+        << ": " << pigpio_error(response) << std::endl;
+    }
+  }
+};
+
+template<typename T>
+bool errored(T const &handle_container) { return handle_container.handle < 0; }
+
+auto create_i2c_reader(int const pi_handle, int const i2c_handle) {
+  return [=](unsigned const reg){
+    int const response{i2c_read_byte_data(pi_handle, i2c_handle, reg)};
+    if (response < 0) {
+      if constexpr (cc::log_errors) std::cerr << "Error reading from "
+        << "Pi " << pi_handle
+        << ", I2C " << i2c_handle
+        << ", register " << reg
+        << ": " << pigpio_error(response) << std::endl;
+      return std::optional<std::uint8_t>{};
+    } else {
+      return std::optional<std::uint8_t>{response & std::uint8_t{0xffu}};
+    }
+  };
+}
+
+template<typename T0, typename T1 = std::uint8_t>
+bool get_flag(T0 const &status, T1 const &flag = 0xffu){
+  return (status & flag) != 0u;
+}
+template<typename T0, typename T1 = std::uint8_t>
+std::optional<bool> get_flag(
+    std::optional<T0> const &status, T1 const &flag = 0xffu){
+  return (status.has_value())
+    ? std::optional<bool>{get_flag(status.value(), flag)}
+    : std::optional<bool>{};
+};
+
+#include "sensors.cpp"
+
+namespace sensors {
+  // Sampling functions, written here manually, because machine-generating them
+  // would involve a lot of complexity for little gain at my scale of operation.
+
+  sensorhub sample_sensorhub(int const pi_handle, int const i2c_handle) {
+    unsigned constexpr reg_ntc_temperature   {0x01u};
+    unsigned constexpr reg_brightness_0      {0x02u};
+    unsigned constexpr reg_brightness_1      {0x03u};
+    unsigned constexpr reg_status            {0x04u};
+    unsigned constexpr reg_dht11_temperature {0x05u};
+    unsigned constexpr reg_dht11_humidity    {0x06u};
+    unsigned constexpr reg_dht11_error       {0x07u};
+    unsigned constexpr reg_bmp280_temperature{0x08u};
+    unsigned constexpr reg_bmp280_pressure_0 {0x09u};
+    unsigned constexpr reg_bmp280_pressure_1 {0x0au};
+    unsigned constexpr reg_bmp280_pressure_2 {0x0bu};
+    unsigned constexpr reg_bmp280_error      {0x0cu};
+    unsigned constexpr reg_motion            {0x0du};
+
+    unsigned constexpr flag_ntc_overrange       {0x01u};
+    unsigned constexpr flag_ntc_error           {0x02u};
+    unsigned constexpr flag_brightness_overrange{0x04u};
+    unsigned constexpr flag_brightness_error    {0x08u};
+
+    auto const read{create_i2c_reader(pi_handle, i2c_handle)};
+
+    auto const status{read(reg_status)};
+
+    std::optional<bool> const ntc_overrange{
+      get_flag(status, flag_ntc_overrange)};
+    std::optional<bool> const ntc_error{
+      get_flag(status, flag_ntc_error)};
+    std::optional<float> const ntc_temperature{[&](){
+      std::optional<float> const ntc_temperature{read(reg_ntc_temperature)};
+      return (!ntc_overrange.value_or(true) and !ntc_error.value_or(true))
+        ? ntc_temperature : std::optional<float>{};
+    }()};
+
+    std::optional<bool> const dht11_error{get_flag(read(reg_dht11_error))};
+    std::optional<float> const dht11_temperature{[&](){
+      std::optional<float> const dht11_temperature{read(reg_dht11_temperature)};
+      return (!dht11_error.value_or(true)) ? dht11_temperature
+                                            : std::optional<float>{};
+    }()};
+    std::optional<float> const dht11_humidity{[&](){
+      std::optional<float> const dht11_humidity{read(reg_dht11_humidity)};
+      return (!dht11_error.value_or(true)) ? dht11_humidity
+                                            : std::optional<float>{};
+    }()};
+
+    std::optional<bool> const bmp280_error{get_flag(read(reg_bmp280_error))};
+    std::optional<float> const bmp280_temperature{[&](){
+      std::optional<float> const bmp280_temperature{
+        read(reg_bmp280_temperature)};
+      return (!bmp280_error.value_or(true)) ? bmp280_temperature
+                                            : std::optional<float>{};
+    }()};
+    std::optional<float> const bmp280_pressure{[&](){
+      auto const bmp280_pressure_0{read(reg_bmp280_pressure_0)};
+      auto const bmp280_pressure_1{read(reg_bmp280_pressure_1)};
+      auto const bmp280_pressure_2{read(reg_bmp280_pressure_2)};
+      if (bmp280_pressure_0.has_value() and
+          bmp280_pressure_1.has_value() and
+          bmp280_pressure_2.has_value() and
+          !bmp280_error.value_or(true))
+        return std::optional<float>{
+          static_cast<std::uint32_t>(bmp280_pressure_0.value()) <<  0u |
+          static_cast<std::uint32_t>(bmp280_pressure_1.value()) <<  8u |
+          static_cast<std::uint32_t>(bmp280_pressure_2.value()) << 16u};
+      else return std::optional<float>{};
+    }()};
+
+    std::optional<bool> const brightness_overrange{
+      get_flag(status, flag_brightness_overrange)};
+    std::optional<bool> const brightness_error{
+      get_flag(status, flag_brightness_error)};
+    std::optional<float> const brightness{[&](){
+      auto const brightness_0{read(reg_brightness_0)};
+      auto const brightness_1{read(reg_brightness_1)};
+      if (brightness_0.has_value() and
+          brightness_1.has_value() and
+          !brightness_overrange.value_or(true) and
+          !brightness_error.value_or(true))
+        return std::optional<float>{
+          static_cast<std::uint32_t>(brightness_0.value()) << 0u |
+          static_cast<std::uint32_t>(brightness_1.value()) << 8u};
+      else return std::optional<float>{};
+    }()};
+
+    std::optional<float> const motion{optional_apply([](auto const x){
+        return (x == 1) ? 1.f : 0.f;
+      }, read(reg_motion))};
+
+    return sensorhub{{},
+      ntc_temperature,
+      ntc_overrange,
+      ntc_error,
+      dht11_temperature,
+      dht11_humidity,
+      dht11_error,
+      bmp280_temperature,
+      bmp280_pressure,
+      bmp280_error,
+      brightness,
+      brightness_overrange,
+      brightness_error,
+      motion};
+  }
 }
 
 // Returns a tick count since the epoch for the given time point
@@ -31,27 +250,11 @@ auto const integral(std::chrono::time_point<Clock, Duration> const &time_point){
 
 //int main(int argc, char *argv[]) {
 int main() {
-  int const pi_handle{cc::use_gpio ? pigpio_start(0, 0) : 0};
+  Pi const pi{};
+  if (errored(pi)) return 1;
 
-  if (pi_handle < 0) {
-    std::cerr
-      << "Could not connect to pigpio daemon: "
-      << pigpio_error(pi_handle)
-      << std::endl;
-    return 1;
-  }
-
-  int const i2c_handle{
-    cc::use_i2c ? i2c_open(pi_handle, 0x1u, 0x17u, 0x0u) : 0};
-
-  if (i2c_handle < 0) {
-    pigpio_stop(pi_handle);
-    std::cerr
-      << "Could not open I2C device: "
-      << pigpio_error(i2c_handle)
-      << std::endl;
-    return 1;
-  }
+  I2C const i2c{pi, 0x1u, 0x17u};
+  if (errored(i2c)) return 1;
 
   std::chrono::high_resolution_clock const clock{};
 
@@ -65,24 +268,32 @@ int main() {
   for (unsigned aggregate_index{0u};
       aggregate_index < cc::aggregates_per_run;
       ++aggregate_index) {
+
+    sensors::sensorhub a0{};
+    sensors::sensorhub_state s0{};
+
+    //sensors::write_field_names(std::cout, a0);
+
     for (unsigned sample_index{0u};
         sample_index < cc::samples_per_aggregate;
         ++sample_index) {
 
-      std::cout
-        << "Aggregate index " << aggregate_index
-        << ", sample index " << sample_index
-        << std::endl;
+      //std::cout
+      //  << "Aggregate index " << aggregate_index
+      //  << ", sample index " << sample_index
+      //  << std::endl;
 
-      int temp{i2c_read_byte_data(pi_handle, i2c_handle, 0x1u)};
-      std::cout << temp << std::endl;
+      auto const x0{sensors::sample_sensorhub(pi, i2c)};
+      std::tie(a0, s0) = aggregation_step(a0, s0, x0);
+      //sensors::write_fields(std::cout, x0);
 
       //int GPIO{4};
-      //int level{gpio_read(pi_handle, GPIO)};
+      //int level{gpio_read(pi, GPIO)};
       //printf("GPIO %d is %d\n", GPIO, level);
 
       if ((sample_index + 1u) == cc::samples_per_aggregate) {
-        // TODO: Do aggregation…
+        a0 = aggregation_finish(a0, s0);
+        sensors::write_fields(std::cout, a0);
       }
 
       std::this_thread::sleep_until(time_reference + (
@@ -90,9 +301,6 @@ int main() {
         cc::sampling_interval));
     }
   }
-
-  i2c_close(pi_handle, i2c_handle);
-  pigpio_stop(pi_handle);
 
   return 0;
 }
